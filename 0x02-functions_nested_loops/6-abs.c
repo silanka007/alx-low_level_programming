e7c61f2d7dc5573fdea475bafb35263b6124ffef
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-12 21:49:39 +0000

    code refactoring

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,13 @@
 
 /**
  * _abs - returns the absolute value of passed int argument
- * Return - integer
+ * @n: passed argument of type integer
+ * Return: integer
  */
 int _abs(int n)
 {
-	if(n > 0)
-		return n;
+	if (n > 0)
+		return (n);
 	else 
-		return 0 - n;
+		return (0 - n);
 }

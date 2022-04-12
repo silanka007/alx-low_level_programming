85eb88b712a4e28aded49e3537a0a2c8254a1b4b
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-12 20:46:34 +0000

    code refactoring

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,7 @@
+#include "main.h"
+
 /**
+ *
  * print_alphabet - prints the alphabets in lowercase
  * Return: always 0 for success
  */
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,7 @@
+#ifndef _main_h_
+#define _main_h_
+
 int _putchar(char c);
 void print_alphabet(void);
+
+#endif

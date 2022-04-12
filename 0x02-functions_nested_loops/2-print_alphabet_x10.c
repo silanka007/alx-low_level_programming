0e03bd94752788793ed8b5baea4952a00a4bed59
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-12 21:09:25 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,15 @@
  * print_alphabet_x10 - Prints alphabet 10 times
  * Return: returns nothing (void)
  */
-
 void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 97; i <=122; i++)
+	for (i = 0; i < 10; i++)
 	{
 		int j;
-		for (j = 0; j < 10; j++) 
+
+		for (j = 97; j <= 122; j++) 
 			_putchar(i);
 		_putchar('\n');
 	}

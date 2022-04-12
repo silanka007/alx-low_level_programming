37145ae1d8d79a81dcea695004d9f5894dd1be52
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-12 18:27:57 +0000

    adjusting to betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,14 @@
 
 /**
  * main prints the string _putchar
- * return: always 0 for success
+ * Return: always 0 for success
 */
 int main(void)
 {
 	char txt[8] = "_putchar";
 	int i;
-	for(i=0; i < 8; i++){
+	for(i = 0; i < 8; i++)
 		_putchar(txt[i]);
-	}
 	_putchar('\n');
 	return (0);
 }

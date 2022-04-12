71c7f29f5242018f03ead162522afbe08f7f0a46
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-12 20:39:03 +0000

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ * Return: always 0 for success
+ */
+int print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar
+ 	return (0);
+}

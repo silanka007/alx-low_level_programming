8c97cbb71f62c77e3fe850c5eb73a78eae0c91ef
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-12 22:15:32 +0000

    11-print_to_98.c :

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ * @n: passed in argument of type integer
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	int stat;
+
+	if(n < 0)
+		stat = 0;
+	else
+		stat = n;
+	while(stat <= 98)
+	{
+		printf("%i, ", stat);
+		stat++;
+	}
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,5 +12,6 @@ int add(int, int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+void print_to_98(int n);
 
 #endif

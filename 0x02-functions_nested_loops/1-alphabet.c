38d550a1aca597c0f08dc6dff03457d000429c2f
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-12 20:47:54 +0000

    code refactoring

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
  * print_alphabet - prints the alphabets in lowercase
  * Return: always 0 for success
  */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	int i;
 
 	for (i = 97; i <= 122; i++)
 		_putchar(i);
 	_putchar('\n');
- 	return (0);
 }

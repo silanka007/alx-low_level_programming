08ec0b8a92555e7b29f34545e2fd8658627265de
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-24 12:21:51 +0100

    code refactoring

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (*(src + i) != '\0' && i < n)
 	{
-		if (*(src + i) != '\0')
-			*(dest + i) = *(src + i);
-		else
-			*(dest + i) = '\0';
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
 	}
 	return (dest);
 }

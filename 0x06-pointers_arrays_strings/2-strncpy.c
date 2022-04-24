ffbcc3a2aee1bc501ba2266617cd1b03b8bff2f1
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-24 12:13:55 +0100

    strcpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,17 +7,19 @@
  * @n: length of src to copy to dest
  * Return: new copied string
  */
-char *_strncopy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		if (*(dest + i) != '\0')
+		{
 			if (*(src + i) != '\0')
 				*(dest + i) = *(src + i);
 			else
 				*(dest + i) = '\0';
+		}
 	}
 	return (dest);
 }

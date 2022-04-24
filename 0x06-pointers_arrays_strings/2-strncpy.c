026bcd08f50e95a14f863ae9e3ec251d3a1acba0
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-24 12:16:59 +0100

    code refactoring

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (*(dest + i) != '\0')
-		{
-			if (*(src + i) != '\0')
-				*(dest + i) = *(src + i);
-			else
-				*(dest + i) = '\0';
-		}
+		if (*(src + i) != '\0')
+			*(dest + i) = *(src + i);
+		else
+			*(dest + i) = '\0';
 	}
 	return (dest);
 }

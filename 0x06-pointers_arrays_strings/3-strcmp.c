896ca1aa6225dbf10ba31ea2df81092d8de0eedd
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-24 12:55:55 +0100

    code refactoring

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	int shouldLoop = 1;
 	int i = 0;
 	int val1;
 	int val2;
 
-	while (true)
+	while (shouldLoop)
 	{
-		val1 = (int) *(s1 + i);
-		val2 = (int) *(s2 + i);
+		val1 = (int)*(s1 + i);
+		val2 = (int)*(s2 + i);
 		if (val2 != val1)
-			return (val2 - val1);
+			shouldLoop = 0;
+		return (val1 - val2);
 		i++;
-	}	
+	}
 	return (0);
 }

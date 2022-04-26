04a43ee4e4117d574bec656ff1b0a05ce79b052c
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-26 18:26:17 +0100

    reverting to formal code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
+ * _memset - writes len value of value b to string s
+ * @s: a string
+ * @b: a character
+ * @n: a integer
+ * Return: a modified string of b
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i;
+  unsigned int i = 0;
 
-  for (i = 0; i < n; i++)
+  while (i < n)
   {
-    s[i] = b;
+    *(s + i) = b;
+    i++;
   }
 
   return (s);

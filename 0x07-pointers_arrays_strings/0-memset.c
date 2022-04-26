0005b3332d7006ff1c166ba1ec749a0775e74fe9
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-04-26 18:15:45 +0100

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - writes len value of value b to string s
+ * @s: a string
+ * @b: a character
+ * @n: a integer
+ * Return: a modified string of b
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+  int i = 0;
+
+  while(i < n)
+  {
+    *(s + i) = b;
+    i++;
+  }
+
+  return *s;
+}

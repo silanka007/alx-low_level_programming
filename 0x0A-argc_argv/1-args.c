a276dea36727a8020c397b195c72c9c0cf743a90
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-05-02 19:41:54 +0100

    refactored 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints the number of command line argument passed to the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
 int main(int argc, char *arg[] __attribute__((unused)))
 {
 	printf("%i\n", argc);

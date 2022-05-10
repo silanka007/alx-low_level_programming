0bdb8309e3848d4667e2033b2e26560f353385b5
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-05-10 19:58:34 +0100

    removal of betty formatting

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,20 +12,21 @@ int *array_range(int min, int max) {
   int *ptr;
   int len;
   int i;
-  int j;
 
   if (min > max)
     return (NULL);
 
-  len = max - min;
-  j = min;
-  ptr = malloc(sizeof(int) * (len + 2));
-  if (!ptr)
+  len = max - min + 1;
+  ptr = malloc(sizeof(min) * len);
+
+  if (ptr == NULL)
     return (NULL);
+
   for (i = 0; i < len; i++) {
-    if (j <= max)
-      ptr[i] = j;
-    j++;
+    if (min <= max)
+      ptr[i] = min;
+    min++;
   }
+
   return (ptr);
 }

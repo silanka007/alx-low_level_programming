0a580c688cdf42900b9dcfbf2ad10094c037b671
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-05-12 15:10:12 +0000

    code refactoring

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,50 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
-
-void print_str(char *key, char *val);
-void print_float(char *key, float val);
 
 /**
  * print_dog - prints a struct dog
- * @d: pointer
- * Return: void
+ * @d: struct dog to print
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		print_str("name", d->name);
-		print_float("age", d->age);
-		print_str("owner", d->owner);
-	}
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
-
-/**
- * print_str - prints one prop
- * @key: the props name
- * @val: the props value
- * Return: void
- */
-void print_str(char *key, char *val)
-{
-	if (val == NULL)
-		printf("%s: (nil)\n", key);
-	else
-		printf("%s: %s\n", key, val);
-}
-
-
-/**
- * print_int - prints one prop
- * @key: the props name
- * @val: the props value
- * Return: void
- */
-void print_float(char *key, float val)
-{
-	if (!val)
-		printf("%s: (nil)\n", key);
-	else
-		printf("%s: %f\n", key, val);
-}
-

06166ca48cf0da6dda352810a60ca2acb616c628
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-06-01 19:42:45 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,10 +2,9 @@
 
 /**
   * create_file - creates a file
-  * @filename: filename 
-  * @text_content: file content 
-  *
-  * Return: int 
+  * @filename: filename
+  * @text_content: file content
+  * Return: int
   */
 int create_file(const char *filename, char *text_content)
 {
